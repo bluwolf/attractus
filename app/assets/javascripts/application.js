8257e7471f641db5297a931f45926a39f99a0dde
Xiaohan Chen <xiaohanc@usc.edu>
2016-06-09 18:16:37 -0700

    assets compieled

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require jquery_ujs
 //= require materialize.min
 //= require bootstrap.min
-//= require turbolinks
 //= require bootstrap-typeahead-rails
-//= require_tree .
+//= require turbolinks
